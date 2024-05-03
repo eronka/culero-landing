b2c0dc8444341cc3087dee3c4b89a9614f079d98
Wiggle59 <hiramjake59@gmail.com>
2024-05-03 16:23:31 +0900

    changed: button event/effect & updated: responsive

--- components/Home/Description/index.tsx
+++ components/Home/Description/index.tsx
@@ -3,8 +3,31 @@ import Image from "next/image"
 import LinkedinIco from '@/assets/linkedin.png';
 import TwitterIco from '@/assets/twitter.png';
 import InstagramIco from '@/assets/instagram.png';
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as Yup from 'yup'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { showToast } from "@/utils/toastUtils";
+
+type Inputs = {
+    email: string
+}
 
 const Description = () => {
+    const formSchema = Yup.object().shape({
+        email: Yup.string()
+            .required('Email is mendatory')
+            .matches(/^(?=.*[@])/, 'Must contain @'),
+    })
+
+    const formOptions = { resolver: yupResolver(formSchema) }
+    const { register, handleSubmit } = useForm(formOptions)
+
+    const   onSubmit: SubmitHandler<Inputs> = async (data) => {
+        console.log("data ", data);
+        await showToast('success', 'Successfully submitted your email!');
+    }
+
+
     return (
         <div className="flex flex-col 2xl:space-y-14 lg:space-y-8 space-y-6 md:w-1/2 w-full">
             <LogoImg />
@@ -22,12 +45,16 @@ const Description = () => {
 
             <div className="flex flex-col space-y-4 xl:text-[24px] lg:text-[22px] text-[18px] xl:leading-[36px] lg:leading-[30px] leading-[26px] font-meidum w-full">
                 <p className="font-bold">Subscribe for Early Access</p>
-                <div className="flex justify-between xl:w-[75%] w-[85%] bg-[#F8F8F8] p-2 rounded-2xl">
+                <form onSubmit={handleSubmit(onSubmit)} className="flex justify-between xl:w-[75%] w-[85%] bg-[#F8F8F8] p-2 rounded-2xl">
                     <div className="flex items-center xl:w-[70%] w-[80%]">
-                        <input type="email" className="w-full italic xl:text-[20px] lg:text-[16px] text-[14px] font-medium lg:px-4 px-2 bg-transparent focus:outline-none text-black" placeholder="Enter email address" />
+                        <input type="email" {...register('email')} className="w-full italic xl:text-[20px] lg:text-[16px] text-[14px] font-medium lg:px-4 px-2 bg-transparent focus:outline-none text-black" placeholder="Enter email address" />
                     </div>
-                    <button className="font-semibold xl:text-[22px] lg:text-[18px] text-[15px] bg-[#1E68FF] rounded-2xl py-2 xl:px-8 lg:px-6 px-4">Subscribe</button>
-                </div>
+                    <input
+                        type="submit"
+                        value="Subscribe"
+                        className="font-semibold xl:text-[22px] lg:text-[18px] text-[15px] bg-[#1E68FF] rounded-2xl py-2 xl:px-8 lg:px-6 px-4 hover:shadow-xl hover:bg-[#507eda]transition duration-500 ease-in-out hover:scale-105 cursor-pointer"
+                    />
+                </form>
             </div>
 
             <div className="flex space-x-4">
--- components/Home/HomeBack/index.tsx
+++ components/Home/HomeBack/index.tsx
@@ -3,8 +3,8 @@ import HomeBackImg from '@/assets/home-back.png';
 
 const HomeBack = () => {
     return (
-        <div className="w-1/2 h-full md:block hidden">
-            <Image src={HomeBackImg} alt="Home Back" className="w-full h-full" />
+        <div className="w-1/2 h-full md:flex items-center hidden bg-auto">
+            <Image src={HomeBackImg} alt="Home Back" className="w-full h-auto" />
         </div>
     )
 }
